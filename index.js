ba58332516752167c93baa04b58f354000c23c1c
Hassan Murtaza <mrhassanmurtaza@gmail.com>
2019-01-25 13:23:09 +0500

    Setting up elastic beanstalk env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require('express');
  
 var app = express();
+
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
  res.send('hello world');
